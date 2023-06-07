7cb4d89a5b14b8ade37cfcfbfbfe449aaaf9693b
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-06-07 10:49:52 +0800

    feat: support change switch dark & light

--- media/main.js
+++ media/main.js
@@ -4,53 +4,61 @@ export function initVue(form = {
   name: '',
   time: '',
   detail: '',
-}) {
+}, switchvalue) {
   const { name = '', time = '', detail = '' } = form
   return `
   const vscode = acquireVsCodeApi()
 
-  new Vue({
-    el: '#app',
-    data() {
-      return {
-        form: {
-          name: "${name}",
-          time: "${time}",
-          detail: "${detail}"
+  setTimeout(()=>{
+    new Vue({
+      el: '#app',
+      data() {
+        return {
+          switchvalue: ${switchvalue},
+          form: {
+            name: "${name}",
+            time: "${time}",
+            detail: "${detail}"
+          },
+        }
+      },
+      methods: {
+        changeMode(){
+          this.switchvalue = !this.switchvalue
+          vscode.postMessage({ type: 'switchMode', value: this.switchvalue })
         },
-      }
-    },
-    methods: {
-      submitForm() {
-        if (!this.form.name) {
-          this.$message({
-            message: '请先填写计划名称!',
-            type: 'error',
-          })
-          vscode.postMessage({ type: 'error', value: '请先填写计划名称!' })
-        }
-        else if (!this.form.time) {
-          this.$message({
-            message: '请先填写开始时间!',
-            type: 'error',
-          })
-          vscode.postMessage({ type: 'error', value: '请先填写开始时间!' })
-        }
-        else {
-          vscode.postMessage({ type: 'submit', value: this.form })
-          this.$message({
-            message: '操作成功',
-            type: 'success',
-          })
-          this.resetForm()
-        }
+        submitForm() {
+          if (!this.form.name) {
+            this.$message({
+              message: '请先填写计划名称!',
+              type: 'error',
+            })
+            vscode.postMessage({ type: 'error', value: '请先填写计划名称!' })
+          }
+          else if (!this.form.time) {
+            this.$message({
+              message: '请先填写开始时间!',
+              type: 'error',
+            })
+            vscode.postMessage({ type: 'error', value: '请先填写开始时间!' })
+          }
+          else {
+            vscode.postMessage({ type: 'submit', value: this.form })
+            this.$message({
+              message: '操作成功',
+              type: 'success',
+            })
+            this.resetForm()
+          }
+        },
+        resetForm() {
+          this.form.name = ''
+          this.form.time = ''
+          this.form.detail = ''
+        },
       },
-      resetForm() {
-        this.form.name = ''
-        this.form.time = ''
-        this.form.detail = ''
-      },
-    },
+    })
   })
+ 
   `
 }
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { calculateTime, compareDay, getCurrentDate, getDayFirst } from './common
 
 let timer: any = null
 let claude: ClaudeApi
+let switchvalue = false
 // 使用webview的方式来增加、修改、查看任务
 export async function activate(context: vscode.ExtensionContext) {
   const { avater, name } = vscode.workspace.getConfiguration('daily-planner')
@@ -16,8 +17,15 @@ export async function activate(context: vscode.ExtensionContext) {
   const provider = new CreateWebview(
     context.extensionUri,
     'Daily planner',
-    ['https://unpkg.com/vue@2/dist/vue.js', 'https://unpkg.com/element-ui/lib/index.js'],
-    ['reset.css', 'https://unpkg.com/element-ui/lib/theme-chalk/index.css', 'main.css'])
+    [{
+      enforce: 'pre',
+      src: 'https://unpkg.com/vue@2/dist/vue.js',
+    }, {
+      enforce: 'pre',
+      src: 'https://unpkg.com/element-ui/lib/index.js',
+    }],
+    ['reset.css', 'https://unpkg.com/element-ui/lib/theme-chalk/index.css', 'main.css'],
+  )
 
   const todoDataProvider = new TodoDataProvider(context, () => {
     if (!isClosed && !todoDataProvider.hasTodo) {
@@ -56,12 +64,15 @@ export async function activate(context: vscode.ExtensionContext) {
       if (type === 'error') {
         vscode.window.showErrorMessage(value)
       }
-      else {
+      else if (type === 'submit') {
         const { name, time, detail } = value
         const processDetail = detail.replace(/\n/g, '\\n')
 
         todoDataProvider.addTodo({ name, time, detail: processDetail })
         vscode.window.showInformationMessage('提交成功')
+      }
+      else if (type === 'switchMode') {
+        switchvalue = value
       }
     })
   })
@@ -75,12 +86,15 @@ export async function activate(context: vscode.ExtensionContext) {
       if (type === 'error') {
         vscode.window.showErrorMessage(value)
       }
-      else {
+      else if (type === 'submit') {
         const { name, time, detail } = value
         const processDetail = detail.replace(/\n/g, '\\n')
 
         todoDataProvider.addDailyTodo({ name, time, detail: processDetail })
         vscode.window.showInformationMessage('提交成功')
+      }
+      else if (type === 'switchMode') {
+        switchvalue = value
       }
     })
   })
@@ -189,6 +203,7 @@ export async function activate(context: vscode.ExtensionContext) {
     data.treeItem.contextValue = 'daily-uncheck'
     todoDataProvider.refresh()
   })
+
   const deleteTodoDisposable = vscode.commands.registerCommand('todoList.deleteTodo', async (todoItem) => {
     if (!todoItem)
       return
@@ -211,7 +226,7 @@ export async function activate(context: vscode.ExtensionContext) {
       if (type === 'error') {
         vscode.window.showErrorMessage(value)
       }
-      else {
+      else if (type === 'submit') {
         const { name, time, detail } = value
         const processDetail = detail.replace(/\n/g, '\\n')
         todoItem.label = todoItem.label.replace(`开始时间: ${todoItem.time}`, `开始时间: ${time}`)
@@ -223,6 +238,9 @@ export async function activate(context: vscode.ExtensionContext) {
         todoDataProvider.updateTodo(todoItem)
         vscode.window.showInformationMessage('修改成功')
         provider.destory()
+      }
+      else if (type === 'switchMode') {
+        switchvalue = value
       }
     }, todoItem)
   })
@@ -238,7 +256,7 @@ export async function activate(context: vscode.ExtensionContext) {
   function createForm(status: 'add' | 'view' | 'edit', callback: (data: any) => void, form: any = {}) {
     provider.deferScript(`
     <script>
-    ${initVue(form)}
+    ${initVue(form, switchvalue)}
     </script>
     `)
     const title = status === 'add'
@@ -247,13 +265,24 @@ export async function activate(context: vscode.ExtensionContext) {
         ? 'Daily Planner View Page'
         : 'Daily Planner Edit Page'
     return provider.create(`
-    <div id="app">
+    <div id="app" :class="[switchvalue && 'dark']">
       <a href="https://github.com/Simon-He95/daily-planner" class="github-corner" aria-label="View source on GitHub">
         <svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true">
           <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
           <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
         </svg>
       </a>
+        <div @click="changeMode" class="mode">
+          <svg v-if="switchvalue" class="inline" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1.2em" height="1.2em" style="vertical-align: sub;">
+            <path fill="currentColor" d="M10 7a7 7 0 0 0 12 4.9v.1c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2h.1A6.979 6.979 0 0 0 10 7zm-6 5a8 8 0 0 0 15.062 3.762A9 9 0 0 1 8.238 4.938A7.999 7.999 0 0 0 4 12z">
+            </path>
+          </svg>
+          <svg v-else class="inline" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1.2em" height="1.2em" style="vertical-align: sub;">
+            <path fill="currentColor" d="M12 18a6 6 0 1 1 0-12a6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8a4 4 0 0 0 0 8zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636L5.636 7.05L3.515 4.93zM16.95 18.364l1.414-1.414l2.121 2.121l-1.414 1.414l-2.121-2.121zm2.121-14.85l1.414 1.415l-2.121 2.121l-1.414-1.414l2.121-2.121zM5.636 16.95l1.414 1.414l-2.121 2.121l-1.414-1.414l2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z">
+            </path>
+          </svg>
+        </div>
+
       <div class="user">
         <div class="bg"></div>
         <div class="avater">
